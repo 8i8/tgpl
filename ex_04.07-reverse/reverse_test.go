538e16d97d80c6854b28d6a211b20b23132ce6dc
8i8 <iainardernhill@gmail.com>
2019-01-15 21:25:20 +0100

    reverse test case altered

--- ex_04.07-reverse/reverse_test.go
+++ ex_04.07-reverse/reverse_test.go
@@ -24,19 +24,27 @@ func TestReverseUtf8(t *testing.T) {
 		t.Errorf("error: received `%v` wanted `%v`.\n", string(s3), string(s2))
 	}
 
-	var s4 []byte = []byte("界世 ,olleH")
-	var s5 []byte = []byte("Hello, 世界")
+	s1 = []byte("界世 ,olleH")
+	s2 = []byte("Hello, 世界")
 
-	s6 := ReverseUtf8(s4)
-	if bytes.Compare(s6, s5) != 0 {
-		t.Errorf("error: received `%v` wanted `%v`.\n", string(s6), string(s5))
+	s3 = ReverseUtf8(s1)
+	if bytes.Compare(s3, s2) != 0 {
+		t.Errorf("error: received `%v` wanted `%v`.\n", string(s3), string(s2))
 	}
 
-	var s7 []byte = []byte("Hello, 世界")
-	var s8 []byte = []byte("界世 ,olleH")
+	s1 = []byte("Hello, 世界")
+	s2 = []byte("界世 ,olleH")
 
-	s9 := ReverseUtf8(s7)
-	if bytes.Compare(s9, s8) != 0 {
-		t.Errorf("error: received `%v` wanted `%v`.\n", string(s9), string(s8))
+	s3 = ReverseUtf8(s1)
+	if bytes.Compare(s3, s2) != 0 {
+		t.Errorf("error: received `%v` wanted `%v`.\n", string(s3), string(s2))
+	}
+
+	s1 = []byte("Hello,世 界 ")
+	s2 = []byte(" 界 世,olleH")
+
+	s3 = ReverseUtf8(s1)
+	if bytes.Compare(s3, s2) != 0 {
+		t.Errorf("error: received `%v` wanted `%v`.\n", string(s3), string(s2))
 	}
 }
