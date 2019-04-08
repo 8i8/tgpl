7f7a74da2137982102e3c3a2360d15e0615ff66e
8i8 <iainardernhill@gmail.com>
2019-04-08 14:45:58 +0200

    basic trie working with multiple goroutines.

--- ex_04.12-xkcd/trie/trie.go
+++ ex_04.12-xkcd/trie/trie.go
@@ -1,52 +1,162 @@
 package trie
 
-type hash map[rune]*Trie
+import (
+	"fmt"
+	"unicode"
+)
+
+type hash map[rune]*node
 
 type Trie struct {
-	r    rune
-	stop bool
+	wc    int // Word count.
+	start *node
+}
+
+type node struct {
+	stop bool // End of a word.
 	next hash
 }
 
-// Add inserts the given string into the trie.
+// Add inserts given string into the trie.
 func (t *Trie) Add(s string) {
 
-	for _, r := range s {
-		if t.next == nil {
-			t.next = make(hash)
-		}
-		if t.next[r] == nil {
-			t.next[r] = new(Trie)
-			t.next[r].r = r
-		}
-		t = t.next[r]
+	if t.start == nil {
+		t.start = new(node)
 	}
-	t.stop = true
+	n := t.start
+
+	for _, r := range s {
+		r = unicode.ToLower(r)
+		if n.next == nil {
+			n.next = make(hash)
+		}
+		if n.next[r] == nil {
+			n.next[r] = new(node)
+		}
+		n = n.next[r]
+	}
+
+	// If the word is not already present, count it.
+	if !n.stop {
+		t.wc++
+		n.stop = true
+	}
 }
 
-// Check verifies if the given string exists in the trie as a complete word.
-func (t *Trie) Check(s string) bool {
+// Check verifies using a single routine if given string exists in the trie as
+// a complete word from the given node.
+func (n *node) Check(s string) bool {
 
+	l := 0
 	for _, r := range s {
-		if t.next[r] == nil {
+		l++
+		if n.next[r] == nil {
 			break
 		}
-		t = t.next[r]
+		n = n.next[r]
+	}
+	if l != len(s) {
+		return false
 	}
 	return true
 }
 
-// SubWord checks if the given string exists as a sub word with the trie.
-func (t *Trie) SubWord(s string) bool {
+func getAllFollowing(t *Trie) {
+}
 
+// Check verifies using multiple goroutines if the given string exists in the
+// trie as a complete word, from the given node.
+func (n *node) GopherCheck(s string, ch chan<- bool) {
+
+	// remove the first rune it has already been noted, it was the key of
+	// the map that calls this function.
+	s = string([]rune(s)[1:])
+
+	for _, r := range s {
+		if n.next[r] == nil {
+			ch <- false
+			return
+		}
+		n = n.next[r]
+	}
+	ch <- true
+	return
+}
+
+// SubWord checks whether the given string exists as a sub word within the
+// trie.
+func (n *node) subWord(s string, ch1 chan<- bool) {
+
+	ch := make(chan bool)
+
+	if n == nil {
+		ch1 <- false
+		return
+	}
+
+	// For each character in the hashmap check the first rune against the
+	// rune hey of the hashmap if it matches, run CheckGopher to search for
+	// the given string.
+	for r, c := range n.next {
+		if r == rune(s[0]) {
+			go c.GopherCheck(s, ch)
+		}
+	}
+	for r, _ := range n.next {
+		if r == rune(s[0]) {
+			out := <-ch
+			if out {
+				ch1 <- out
+				return
+			}
+		}
+	}
+
+	// Move forwards one character and repeat for every character in the
+	// map.
+	for _, n1 := range n.next {
+		go n1.subWord(s, ch)
+	}
+	for range n.next {
+		out := <-ch
+		if out {
+			ch1 <- out
+			return
+		}
+	}
+
+	ch1 <- false
+	return
+}
+
+// SubWord starts a multi goroutine trie search for the given string.
+func (t *Trie) SubWord(s string) {
+
+	n := t.start
+	ch := make(chan bool)
 	var out bool
-	for _, h := range t.next {
-		out = h.Check(s)
+
+	// Send a goroutine.
+	go n.subWord(s, ch)
+	out = <-ch
+	printState(s, out)
+}
+
+func printState(s string, out bool) {
+
+	if out {
+		fmt.Printf("string '%s' found.\n", s)
+	} else {
+		fmt.Printf("string '%s' not found.\n", s)
 	}
-	for _, h := range t.next {
-		u := h.next
-		r := h.r
-		out = u[r].SubWord(s)
-	}
-	return out
+}
+
+func main() {
+
+	trie := Trie{}
+
+	trie.Add("hello")
+	trie.SubWord("l")
+	trie.Add("world")
+	trie.SubWord("l")
 }
