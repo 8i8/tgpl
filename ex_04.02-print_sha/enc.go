00cf36a1a2f4c0dc0c69a1c910cf044a33f148ad
8i8 <iainardernhill@gmail.com>
2020-07-18 09:46:34 +0200

    enc modified

--- ex_04.02-print_sha/enc.go
+++ ex_04.02-print_sha/enc.go
@@ -13,9 +13,19 @@ import (
 	"github.com/atotto/clipboard"
 )
 
-// Input flags.
+// MD5 Input flags.
+var MD5 = flag.Bool("md5", false, "Output a md5 hash.")
+
+// NEWLINE Input flags.
+var NEWLINE = flag.Bool("n", false, "Output the hash generated when a newline char is included in the string to be hashed.")
+
+// SHA256 Input flags.
 var SHA256 = flag.Bool("sha256", false, "Output a sha512 hash.")
+
+// SHA384 Input flags.
 var SHA384 = flag.Bool("sha384", false, "Output a sha384 hash.")
+
+// SHA512 Input flags.
 var SHA512 = flag.Bool("sha512", false, "Output a sha512 hash.")
 
 func main() {
@@ -35,25 +45,32 @@ func main() {
 		if len(input) == 0 {
 			return
 		}
+		if *NEWLINE {
+			input = append(input, '\n')
+		}
 
 		// Set the appropriate hash for the input, the given flag or lack there
 		// of, set the hashing algorithm. The resulting hash is copied to the system
 		// clipboard and then written to stdout.
-		if *SHA256 {
-			str := sha256.Sum256([]byte(input))
+		if *MD5 {
+			str := md5.Sum(input)
+			clipboard.WriteAll(fmt.Sprintf("%x", str))
+			fmt.Printf("%x\n", str)
+		} else if *SHA256 {
+			str := sha256.Sum256(input)
 			clipboard.WriteAll(fmt.Sprintf("%x", str))
 			fmt.Printf("%x\n", str)
 		} else if *SHA384 {
-			str := sha512.Sum384([]byte(input))
+			str := sha512.Sum384(input)
 			clipboard.WriteAll(fmt.Sprintf("%x", str))
 			fmt.Printf("%x\n", str)
 		} else if *SHA512 {
-			str := sha512.Sum512([]byte(input))
+			str := sha512.Sum512(input)
 			clipboard.WriteAll(fmt.Sprintf("%x", str))
 			fmt.Printf("%x\n", str)
 		} else {
 			// Default hash is md5
-			str := md5.Sum([]byte(input))
+			str := md5.Sum(input)
 			clipboard.WriteAll(fmt.Sprintf("%x", str))
 			fmt.Printf("%x\n", str)
 		}
