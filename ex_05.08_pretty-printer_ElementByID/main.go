f86378f7bc387d6cd65b97d6dcb92083645bf6b6
8i8 <iainardernhill@gmail.com>
2020-07-25 10:35:54 +0200

    Pretty Printer find eliment by id improved

--- ex_05.08_pretty-printer_ElementByID/main.go
+++ ex_05.08_pretty-printer_ElementByID/main.go
@@ -28,8 +28,8 @@ func main() {
 			fmt.Fprintf(os.Stderr, "stream: html.Parse: %s\n", err)
 			os.Exit(1)
 		}
-		n := ElementByID(doc, "findMe")
-		fmt.Println(n)
+		ElementByID(doc, "findMe")
+		goto exit
 	}
 	// If there are args, assume that they are urls.
 	for _, url := range os.Args[1:] {
@@ -43,8 +43,8 @@ func main() {
 			err = fmt.Errorf("args: html.Parse: %s", err)
 			return
 		}
-		n := ElementByID(doc, "findMe")
-		fmt.Println(n)
+		ElementByID(doc, "findMe")
+		goto exit
 	}
 	if len(os.Args) == 1 {
 		doc, err := html.Parse(bytes.NewReader([]byte(testPage)))
@@ -53,16 +53,24 @@ func main() {
 			os.Exit(1)
 		}
 		// An eliment with the id findMe has been created in testPage.
-		n := ElementByID(doc, "findMe")
-		fmt.Println(n)
+		id := "findMe"
+		ElementByID(doc, id)
 	}
+exit:
 }
 
 var found bool
 
 // ElementByID returns the first node that it reaches with the given id.
 func ElementByID(doc *html.Node, id string) *html.Node {
-	return forEachNode(doc, seek, nil, id)
+	n := forEachNode(doc, seek, nil, id)
+	switch {
+	case n != nil:
+		fmt.Printf("An eliment with the id %q was found\n", id)
+	case n == nil:
+		fmt.Printf("No eliment with the id %q could be found\n", id)
+	}
+	return n
 }
 
 // forEachNode iterates over an HTML document tree.
@@ -70,7 +78,9 @@ func forEachNode(n *html.Node,
 	pre, post func(n *html.Node, id string) bool, id string) *html.Node {
 
 	if pre != nil {
-		found = pre(n, id)
+		if !found {
+			found = pre(n, id)
+		}
 		if found {
 			return n
 		}
@@ -112,3 +122,94 @@ func seek(n *html.Node, id string) bool {
 	}
 	return false
 }
+
+var testPage = `<!DOCTYPE html>
+<html lang="en-gb">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="robots" content="noindex, nofollow" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>
+      home
+    </title>
+    <link rel="preload" href="/assets/javascript/scripts.js" />
+    <link rel="preload" href="/assets/css/main.css" />
+    <link rel="stylesheet" href="/assets/css/main.css" />
+    <script src="/assets/javascript/scripts.js">
+    </script>
+  </head>
+  <body>
+    <!-- This is a comment to test that comments are also resotred. -->
+    <script>
+      beforeLoad();
+    </script>
+    <header>
+      <a href="/fractal">
+        <object type="image/svg+xml" data="/assets/images/Om.svg" width="80" height="80">
+          <img src="/assets/images/Om.svg" alt="AUM" width="80" height="80" />
+        </object>
+      </a>
+      <h1>
+        Jyoḍi
+      </h1>
+    </header>
+    <div class="all-site-wrapper">
+      <nav class="black-grad">
+        <ul>
+          <li>
+            <a href="/nav/jyoti.html">
+              <object id="findMe" type="image/svg+xml" data="/assets/images/jyoti.bw.svg" width="80" height="80">
+                <img src="/assets/images/jyoti.bw.svg.png" alt="What on earth" />
+              </object>
+            </a>
+          </li>
+          <li>
+            <a href="/nav/nakshatra.html">
+              <object type="image/svg+xml" data="/assets/images/nakshatra.bw.svg" width="80" height="80">
+                <img src="/assets/images/nakshatra.bw.svg.png" alt="nakshatra" />
+              </object>
+            </a>
+          </li>
+          <li>
+            <a href="/nav/graha.html">
+              <object type="image/svg+xml" data="/assets/images/graha.bw.svg" width="80" height="80">
+                <img src="/assets/images/graha.bw.svg.png" alt="graha" />
+              </object>
+            </a>
+          </li>
+          <li>
+            <a href="/nav/rasi.html">
+              <object type="image/svg+xml" data="/assets/images/rasi.bw.svg" width="80" height="80">
+                <img src="/assets/images/rasi.bw.svg.png" alt="rasi" />
+              </object>
+            </a>
+          </li>
+          <li>
+            <a href="/nav/bhava.html">
+              <object type="image/svg+xml" data="/assets/images/bhava.bw.svg" width="80" height="80">
+                <img src="/assets/images/bhava.bw.svg.png" alt="bhava" />
+              </object>
+            </a>
+          </li>
+          <li>
+            <a href="/nav/books.html">
+              <object type="image/svg+xml" data="/assets/images/books.bw.svg" width="80" height="80">
+                <img src="/assets/images/books.bw.svg.png" alt="info" />
+              </object>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="index">
+      </div>
+      <article>
+      </article>
+      <aside>
+      </aside>
+    </div>
+    <footer>
+      Page footer
+    </footer>
+  </body>
+</html>
+`
