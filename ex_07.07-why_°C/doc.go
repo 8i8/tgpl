0f9e77f89e464d6ac8b0f0f7a1eb4ed43bed9681
8i8 <iain.coding@gmail.com>
2021-06-15 11:39:33 +0200

    ex_07.07-why_°C

---
+++ ex_07.07-why_°C/doc.go
@@ -0,0 +1,6 @@
+// Exercise 7.7: Explain why the help message contins °C when the
+// default value of 20.0 does not.
+//
+// The help massage continas °C due to its being written in the output
+// of the Stringer method on the Celcius type.
+package main
