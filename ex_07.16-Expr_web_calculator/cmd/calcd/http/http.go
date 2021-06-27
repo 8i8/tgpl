33e616c5aac1783fc365d2888a70831acf1d4003
8i8 <iain.coding@gmail.com>
2021-06-27 09:52:30 +0200

    ex_07.16-Expr_web_calculator

--- ex_07.16-Expr_web_calculator/cmd/calcd/http/http.go
+++ ex_07.16-Expr_web_calculator/cmd/calcd/http/http.go
@@ -2,39 +2,82 @@ package http
 
 import (
 	"Expr_web_calculator/eval"
-	"Expr_web_calculator/svg"
+	"errors"
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"net/http"
 	"os"
 	"os/signal"
+	"text/template"
 )
 
-func calc(e eval.Expr, p svg.Plotter) http.HandlerFunc {
+type Plotter interface {
+	Surface(io.Writer, func(x, y float64) float64)
+}
+
+var templ = template.Must(template.ParseFiles("./assets/templ.gohtml"))
+
+var errVar = errors.New("undefined variable")
+
+func plot(res http.ResponseWriter, req *http.Request, plot Plotter, str string) {
+	expr, err := eval.Parse(str)
+	if err != nil {
+		http.Error(res, err.Error(),
+			http.StatusNotAcceptable)
+		return
+	}
+
+	// Check which variables have been used.
+	vars := make(eval.Vars)
+	if err := expr.Check(vars); err != nil {
+		http.Error(res, err.Error(),
+			http.StatusNotAcceptable)
+		return
+	}
+
+	// Set environment with given variables.
+	for v := range vars {
+		if v.String() != "x" && v.String() != "y" &&
+			v.String() != "r" {
+			http.Error(res, errVar.Error()+": "+v.String(),
+				http.StatusNotAcceptable)
+			return
+		}
+	}
+
+	res.Header().Set("Content-Type", "image/svg+xml")
+	plot.Surface(res, func(x, y float64) float64 {
+		r := math.Hypot(x, y) // distance from (0,0)
+		return expr.Eval(eval.Env{"x": x, "y": y, "r": r}).Float()
+	})
+}
+
+func calc(p Plotter) http.HandlerFunc {
 	return func(res http.ResponseWriter, req *http.Request) {
 
-		// Retrieve data from request.
-		req.ParseForm()
-		expr := req.Form.Get("expr")
-
 		// Set up expression parser.
-		expr, err := eval.Parse(expr)
-		env := eval.NewEnv()
-
-		vars := map[eval.Var]bool
-		err := e.Check(vars)
-		if err != nil {
-			http.Error(res, err, http.StatusNotAcceptable)
+		req.ParseForm()
+		if str := req.Form.Get("expr"); len(str) > 0 {
+			plot(res, req, p, str)
+			return
 		}
-
-		io.WriteString(res, "hello world")
 	}
 }
 
-func Serve(e eval.Expr, p svg.Plotter) {
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	err := templ.ExecuteTemplate(w, "main", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Serve(plot Plotter) {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/calc", calc(e, p))
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/calc", calc(plot))
 	err := http.ListenAndServe(":8000", mux)
 	if err != nil {
 		log.Fatal(err)
--- ex_07.16-Expr_web_calculator/cmd/calcd/main.go
+++ ex_07.16-Expr_web_calculator/cmd/calcd/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"Expr_web_calculator/cmd/calcd/http"
-	"Expr_web_calculator/eval"
 	"Expr_web_calculator/svg"
 )
 
 func main() {
-	var e eval.Expr
-	var p svg.Plotter
-	http.Serve(e, p)
+	http.Serve(svg.NewIsoSurface())
 }
