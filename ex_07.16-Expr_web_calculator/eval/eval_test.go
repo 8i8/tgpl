076ceec0ccd0b9d6b4380623f24f1471e97db56a
8i8 <iain.coding@gmail.com>
2021-07-02 19:12:32 +0200

    eval tests updated for simplification of Calc struct to string

--- ex_07.16-Expr_web_calculator/eval/eval_test.go
+++ ex_07.16-Expr_web_calculator/eval/eval_test.go
@@ -7,32 +7,26 @@ import (
 )
 
 func TestEval(t *testing.T) {
+	const fname = "TestEval"
 	tests := []struct {
 		expr string
 		env  Env
 		want string
 	}{
-		{"sqrt(A / pi)", Env{m: map[Var]float64{"A": 87616, "pi": math.Pi}}, "167"},
-		{"pow(x, 3) + pow(y, 3)", Env{m: map[Var]float64{"x": 12, "y": 1}}, "1729"},
-		{"pow(x, 3) + pow(y, 3)", Env{m: map[Var]float64{"x": 9, "y": 10}}, "1729"},
-		{"5 / 9 * (F - 32)", Env{m: map[Var]float64{"F": -40}}, "-40"},
-		{"5 / 9 * (F - 32)", Env{m: map[Var]float64{"F": 32}}, "0"},
-		{"5 / 9 * (F - 32)", Env{m: map[Var]float64{"F": 212}}, "100"},
+		{"sqrt(A / pi)", map[Var]float64{"A": 87616, "pi": math.Pi}, "167"},
+		{"pow(x, 3) + pow(y, 3)", map[Var]float64{"x": 12, "y": 1}, "1729"},
+		{"pow(x, 3) + pow(y, 3)", map[Var]float64{"x": 9, "y": 10}, "1729"},
+		{"5 / 9 * (F - 32)", map[Var]float64{"F": -40}, "-40"},
+		{"5 / 9 * (F - 32)", map[Var]float64{"F": 32}, "0"},
+		{"5 / 9 * (F - 32)", map[Var]float64{"F": 212}, "100"},
 	}
-	var prevExpr string
 	for _, test := range tests {
-		// Print expr only when it changes.
-		if test.expr != prevExpr {
-			fmt.Printf("\n%s\n", test.expr)
-			prevExpr = test.expr
-		}
 		expr, err := Parse(test.expr)
 		if err != nil {
 			t.Error(err) // parse error
 			continue
 		}
 		got := fmt.Sprintf("%.6g", expr.Eval(test.env).Float())
-		fmt.Printf("\t%v => %s\n", test.env, got)
 		if got != test.want {
 			t.Errorf("%s.Eval() in %v = %q, want %q\n",
 				test.expr, test.env, got, test.want)
@@ -41,17 +35,18 @@ func TestEval(t *testing.T) {
 }
 
 func TestStringer(t *testing.T) {
+	const fname = "TestStringer"
 	tests := []struct {
 		expr string
 		env  Env
 		want string
 	}{
-		{"sqrt(A / pi)", Env{m: map[Var]float64{"A": 87616, "pi": math.Pi}}, "167"},
-		{"pow(x, 3) + pow(y, 3)", Env{m: map[Var]float64{"x": 12, "y": 1}}, "1729"},
-		{"pow(x, 3) + pow(y, 3)", Env{m: map[Var]float64{"x": 9, "y": 10}}, "1729"},
-		{"5 / 9 * (F - 32)", Env{m: map[Var]float64{"F": -40}}, "-40"},
-		{"5 / 9 * (F - 32)", Env{m: map[Var]float64{"F": 32}}, "0"},
-		{"5 / 9 * (F - 32)", Env{m: map[Var]float64{"F": 212}}, "100"},
+		{"sqrt(A / pi)", map[Var]float64{"A": 87616, "pi": math.Pi}, "167"},
+		{"pow(x, 3) + pow(y, 3)", map[Var]float64{"x": 12, "y": 1}, "1729"},
+		{"pow(x, 3) + pow(y, 3)", map[Var]float64{"x": 9, "y": 10}, "1729"},
+		{"5 / 9 * (F - 32)", map[Var]float64{"F": -40}, "-40"},
+		{"5 / 9 * (F - 32)", map[Var]float64{"F": 32}, "0"},
+		{"5 / 9 * (F - 32)", map[Var]float64{"F": 212}, "100"},
 	}
 	var prevExpr string
 	for _, test := range tests {
@@ -71,11 +66,9 @@ func TestStringer(t *testing.T) {
 			t.Error(err) // parse error
 			continue
 		}
-		got := fmt.Sprintf("%.6g", expr.Eval(test.env).Float())
-		fmt.Printf("\t%v => %s\n", test.env, got)
+		got := fmt.Sprintf("%.f", expr.Eval(test.env).Float())
 		if got != test.want {
-			t.Errorf("%s.Eval() in %v = %q, want %q\n",
-				test.expr, test.env, got, test.want)
+			t.Errorf("%s: got %q want %q\n", fname, got, test.want)
 		}
 	}
 }
@@ -86,41 +79,34 @@ func TestEnv(t *testing.T) {
 		env  Env
 		want string
 	}{
-		{"sqrt(A / pi)", Env{m: map[Var]float64{"A": 87616, "pi": math.Pi}}, "167"},
-		{"pow(x, 3) + pow(y, 3)", Env{m: map[Var]float64{"x": 12, "y": 1}}, "1729"},
-		{"pow(x, 3) + pow(y, 3)", Env{m: map[Var]float64{"x": 9, "y": 10}}, "1729"},
-		{"5 / 9 * (F - 32)", Env{m: map[Var]float64{"F": -40}}, "-40"},
-		{"5 / 9 * (F - 32)", Env{m: map[Var]float64{"F": 32}}, "0"},
-		{"5 / 9 * (F - 32)", Env{m: map[Var]float64{"F": 212}}, "100"},
+		{"sqrt(A / pi)", map[Var]float64{"A": 87616}, "167.000117"},
+		{"pow(x, 3) + pow(y, 3)", map[Var]float64{"x": 12, "y": 1}, "1729.000000"},
+		{"pow(x, 3) + pow(y, 3)", map[Var]float64{"x": 9, "y": 10}, "1729.000000"},
+		{"5 / 9 * (F - 32)", map[Var]float64{"F": -40}, "-40.000000"},
+		{"5 / 9 * (F - 32)", map[Var]float64{"F": 32}, "0.000000"},
+		{"5 / 9 * (F - 32)", map[Var]float64{"F": 212}, "100.000000"},
 	}
-	var prevExpr string
 	for _, test := range tests {
-		// Print expr only when it changes.
-		if test.expr != prevExpr {
-			fmt.Printf("\n%s\n", test.expr)
-			prevExpr = test.expr
-		}
 		expr, err := Parse(test.expr)
 		if err != nil {
 			t.Error(err) // parse error
 			continue
 		}
 
-		vars := make(map[Var]bool)
+		vars := NewCheckList()
 		err = expr.Check(vars)
 		if err != nil {
 			t.Error(err) // parse error
 			continue
 		}
-		for v, _ := range test.env.m {
-			if !vars[v] {
+		for v, _ := range test.env {
+			if !vars.vars[v] {
 				t.Errorf("%s not found in vars\n", v)
 				continue
 			}
 		}
 
-		got := fmt.Sprintf("%.6g", expr.Eval(test.env).Float())
-		fmt.Printf("\t%v => %s\n", test.env, got)
+		got := fmt.Sprintf("%3f", expr.Eval(test.env).Float())
 		if got != test.want {
 			t.Errorf("%s.Eval() in %v = %q, want %q\n",
 				test.expr, test.env, got, test.want)
